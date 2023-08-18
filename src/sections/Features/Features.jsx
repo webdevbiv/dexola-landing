113d1193bcc480f1c2ba31d48f1312b764ebaa24
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-08-19 01:30:20 +0300

    ADD | map ul items

--- src/sections/Features/Features.jsx
+++ src/sections/Features/Features.jsx
@@ -7,11 +7,40 @@ import img03 from "../../images/star-runner/03.png";
 import arrow from "../../images/icons/button-arrow.svg";
 import { Title } from "../../components/Title/Title";
 
+const cards = [
+  {
+    img: img01,
+    alt: "star runner nft image",
+    number: "01",
+    title: "STRU Token sale",
+    text: "The StarRunner Token the exclusive crypto currency fueling the adventure of the Play-to-Earn sensation.",
+    link: "",
+  },
+  {
+    img: img02,
+    alt: "star runner nft image",
+    number: "02",
+    title: "Staking",
+    text: "Join a thriving community of stakers, united by their passion for exploration, strategy, and gaming.",
+    link: "",
+  },
+  {
+    img: img03,
+    alt: "star runner nft image",
+    number: "03",
+    title: "NFT minting",
+    text: "Every StarRunner NFT tells a story – YOUR story. This NFT encapsulates the essence of your journey like never before.",
+    link: "",
+  },
+];
 export const Features = () => {
   return (
     <Section id={"features"}>
       <Container>
-        <Title text={"Features"} number={"01"} />
+        <Title
+          text={"Features"}
+          number={"01"}
+        />
         <div className={s.description}>
           <h3 className={s.subTitle}>About StarRunner</h3>
           <p className={s.text}>
@@ -21,63 +50,40 @@ export const Features = () => {
           </p>
         </div>
         <ul className={s.cardsList}>
-          <li className={s.card}>
-            <img src={img01} alt="star runner nft" />
-            <div className={s.cardContent}>
-              <h4>
-                <span>01</span>
-                <span>STRU Token sale</span>
-              </h4>
-              <p>
-                The StarRunner Token the exclusive crypto currency fueling the
-                adventure of the Play-to-Earn sensation.
-              </p>
-              <div className={s.buttonWrapper}>
-                <button className={s.button}>
-                  <img src={arrow} alt="join-now" className={s.arrow} />
-                  <span>discover now</span>
-                </button>
+          {cards.map((card) => (
+            <li
+              className={s.card}
+              key={card.number}
+            >
+              <img
+                src={card.img}
+                alt={card.alt}
+              />
+              <div className={s.cardContent}>
+                <h4>
+                  <span>{card.number}</span>
+                  <span>{card.title}</span>
+                </h4>
+                <p>{card.text}</p>
+                <div className={s.buttonWrapper}>
+                  <a
+                    src={card.link}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <button className={s.button}>
+                      <img
+                        src={arrow}
+                        alt='join-now'
+                        className={s.arrow}
+                      />
+                      <span>discover now</span>
+                    </button>
+                  </a>
+                </div>
               </div>
-            </div>
-          </li>
-          <li className={s.card}>
-            <img src={img02} alt="star runner nft" />
-            <div className={s.cardContent}>
-              <h4>
-                <span>02</span>
-                <span>Staking</span>
-              </h4>
-              <p>
-                Join a thriving community of stakers, united by their passion
-                for exploration, strategy, and gaming.
-              </p>
-              <div className={s.buttonWrapper}>
-                <button className={s.button}>
-                  <img src={arrow} alt="join-now" className={s.arrow} />
-                  <span>discover now</span>
-                </button>
-              </div>
-            </div>
-          </li>
-          <li className={s.card}>
-            <img src={img03} alt="star runner nft" />
-            <div className={s.cardContent}>
-              <h4>
-                <span>03</span>
-                <span>NFT minting</span>
-              </h4>
-              <p>
-                Every StarRunner NFT tells a story – YOUR story. This NFT
-                encapsulates the essence of your journey like never before.
-              </p>
-              <div className={s.buttonWrapper}>
-                <button className={s.button}>
-                  <img src={arrow} alt="join-now" className={s.arrow} />
-                  <span>discover now</span>
-                </button>
-              </div>
-            </div>
-          </li>
+            </li>
+          ))}
         </ul>
       </Container>
     </Section>
--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -5,6 +5,13 @@ import instagram from "../../images/icons/social-instagram.svg";
 import discord from "../../images/icons/social-discord.svg";
 import telegram from "../../images/icons/social-telegram.svg";
 
+const socialLinks = [
+  { icon: facebook, link: "", name: "facebook  icon image" },
+  { icon: instagram, link: "", name: "instagram icon image" },
+  { icon: discord, link: "", name: "discord icon image" },
+  { icon: telegram, link: "", name: "telegram icon image" },
+];
+
 export const Footer = () => {
   return (
     <footer>
@@ -12,32 +19,28 @@ export const Footer = () => {
         <Container>
           <div className={s.wrapper}>
             <p className={s.text}>Designed by Dexola - 2023</p>
-
             <div className={s.divider}>
               <ul className={s.list}>
-                <li className={s.listItem}>
-                  <a>
-                    <img src={facebook} alt="facebook" className={s.icon} />
-                  </a>
-                </li>
-                <li className={s.listItem}>
-                  <a>
-                    <img src={instagram} alt="instagram" className={s.icon} />
-                  </a>
-                </li>
-                <li className={s.listItem}>
-                  <a>
-                    <img src={discord} alt="discord" className={s.icon} />
-                  </a>
-                </li>
-                <li className={s.listItem}>
-                  <a>
-                    <img src={telegram} alt="telegram" className={s.icon} />
-                  </a>
-                </li>
+                {socialLinks.map((item, index) => (
+                  <li
+                    className={s.listItem}
+                    key={index}
+                  >
+                    <a
+                      href={item.link}
+                      target='_blank'
+                      rel='noreferrer noopener'
+                    >
+                      <img
+                        src={item.icon}
+                        alt={item.name}
+                        className={s.icon}
+                      />
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
-
             <p className={s.text}>© All rights reserved</p>
           </div>
         </Container>
