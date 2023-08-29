2e1b01afc677191abcbddc0334f78936ef863dfd
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-08-29 19:42:14 +0300

    UPD | might fix loading time

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -73,13 +73,13 @@ export const Hero = () => {
             className={s.collectionImages}
           >
             <ImageFader
-              images={hero01}
+              images={windowWidth < 500 ? mobile01 : hero01}
               delay={0}
               placeholder={mobile01[0]}
             />
             <ImageFader
               // images={windowWidth < 744 ? mobile02 : tablet02}
-              images={hero02}
+              images={windowWidth < 500 ? mobile02 : hero02}
               placeholder={mobile02[0]}
               delay={1000}
             />
