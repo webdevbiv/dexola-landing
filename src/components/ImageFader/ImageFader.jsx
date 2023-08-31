bf8fec02ca9f80f1be3d2d8a2570e3d6f6106998
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-08-31 09:34:35 +0300

    ADD | Skeleton for images

--- src/components/ImageFader/ImageFader.jsx
+++ src/components/ImageFader/ImageFader.jsx
@@ -1,36 +1,91 @@
+// import PropTypes from "prop-types";
+// import { useState, useEffect } from "react";
+// import s from "./ImageFader.module.scss";
+
+// export const ImageFader = ({ images, delay }) => {
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+//   useEffect(() => {
+//     const startInterval = () => {
+//       return setInterval(() => {
+//         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+//       }, 4000); // 4 seconds for each image
+//     };
+
+//     const timeout = setTimeout(startInterval, delay);
+
+//     return () => {
+//       clearTimeout(timeout); // Cleanup timeout
+//       clearInterval(startInterval); // Cleanup interval
+//     };
+//   }, [images, delay]);
+
+//   return (
+//     <div className={s.imageContainer}>
+//       {images.map((src, index) => (
+//         <img
+//           key={index}
+//           src={src}
+//           alt={`Nft image ${index}`}
+//           className={`${s.image} ${
+//             index === currentImageIndex ? s.active : ""
+//           }`}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// ImageFader.propTypes = {
+//   images: PropTypes.arrayOf(PropTypes.string).isRequired,
+//   delay: PropTypes.number.isRequired,
+// };
+
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import s from "./ImageFader.module.scss";
+import { Skeleton } from "../Skeleton/Skeleton";
 
 export const ImageFader = ({ images, delay }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [loadedImages, setLoadedImages] = useState([]);
 
   useEffect(() => {
-    const startInterval = () => {
-      return setInterval(() => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 4000); // 4 seconds for each image
-    };
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 4000); // 4 seconds for each image
 
-    const timeout = setTimeout(startInterval, delay);
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+    }, delay);
 
     return () => {
       clearTimeout(timeout); // Cleanup timeout
-      clearInterval(startInterval); // Cleanup interval
+      clearInterval(interval); // Cleanup interval
     };
   }, [images, delay]);
+
+  const handleImageLoad = (index) => {
+    setLoadedImages((prev) => [...prev, index]);
+  };
 
   return (
     <div className={s.imageContainer}>
       {images.map((src, index) => (
-        <img
+        <div
           key={index}
-          src={src}
-          alt={`Nft image ${index}`}
-          className={`${s.image} ${
-            index === currentImageIndex ? s.active : ""
-          }`}
-        />
+          className={s.imageWrapper}
+        >
+          <img
+            src={src}
+            alt={`Nft image ${index}`}
+            onLoad={() => handleImageLoad(index)}
+            className={`${s.image} ${
+              index === currentImageIndex ? s.active : ""
+            } `}
+          />
+          {loadedImages.includes(index) && <Skeleton />}
+        </div>
       ))}
     </div>
   );
---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,5 @@
+import s from "./Skeleton.module.scss";
+
+export const Skeleton = () => {
+  return <div className={s.skeleton}></div>;
+};
